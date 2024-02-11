e2d76af88453e7cf532e0c5249ddcd3628cce42d
Harald Ringvold <harald.ringvold@gmail.com>
2024-02-11 23:18:25 +0100

    Add checksum file

--- checksum-Elixir.Rijndael.exs
+++ checksum-Elixir.Rijndael.exs
@@ -1,3 +1,7 @@
 %{
-  "librijndael-v0.1.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:5a6ee4835949f455330ee995620f20e69f6d0c705c54fd9eb61f3cb95d68f07f",
+  "librijndael-v0.1.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:735a946812023733732b2ff19a5fde39f92d716783de3189c0f8516f840e7f93",
+  "librijndael-v0.1.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b0db270e10ee815bd30093266c6bae2c8ef8ec52d49edf411cdc95dc96b856bb",
+  "librijndael-v0.1.4-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:2dca024ae55d324a651322045b3c38a346ac056042b68336c828b3943be11789",
+  "librijndael-v0.1.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8ddcc8fe0d71790499823de15480d64792b6eeb31945e0602882f81446f6dc68",
+  "librijndael-v0.1.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:146c5006d6fba65a7139ff9dcdd3ef9f04b99f3abeb2c5a62ea695dc58779634",
 }
